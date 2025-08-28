fcdbe9dff27114cb4201516462e4d5f6ab262d56
bruno-lima1504 <bruno.lima1504@gmail.com>
2025-08-28 11:32:52 -0300

    feat: add tests and pipeline of CI

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
+  preset: "ts-jest",
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  testMatch: [
+    "<rootDir>/test/**/*.test.(ts|tsx|js)",
+    "<rootDir>/src/**/*.test.(ts|tsx|js)",
+  ],
+  collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/*.d.ts"],
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
